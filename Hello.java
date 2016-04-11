8a81272aed3c4a90b03d3f46a8c832755f4b9dc2
whsun <449832408@qq.com>
2016-04-12 00:00:21 +0800

    Modified Hello.java File

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public Class Hello{
 	public static void main(String[] args){
-		System.out.println("www.yootk.com");
+		System.out.println("Happy New Year");
+		System.out.println("Happy MySelf");
 	}
 }
