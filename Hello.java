1411171e34ac16090865d7f8c791d6aedcb0b6a7
whsun <449832408@qq.com>
2016-04-17 23:34:33 +0800

    url print

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public Class Hello{
 	public static void main(String[] args){
-		System.out.println("*************");
-		System.out.println("##############");
+		System.out.println("www.yootk.com");
+		System.out.println("www.mldnjava.cn");
+		System.out.println("bbs.mldn.cn");
 	}
 
 }
