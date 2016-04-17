f13b4e881fe91d2004b808abc6796aab61a48e7e
whsun <449832408@qq.com>
2016-04-17 23:16:37 +0800

    add static attribute

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public Class Hello{
 	public static void main(String[] args){
+		public static final String COMPANY_NAME = "yootk";
 		System.out.println("Happy New Year sun");
 		System.out.println("Happy MySelf sun");
 	}
