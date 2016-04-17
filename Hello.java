34d8cf270762cbbc6ee2f062d7c2b89ed5eaeee3
whsun <449832408@qq.com>
2016-04-17 23:32:13 +0800

    hello print

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public Class Hello{
 	public static void main(String[] args){
-		System.out.println("*************");
-		System.out.println("##############");
+		System.out.println("Hello World");
+		System.out.println("Hello Yootk");
 	}
 
 }
