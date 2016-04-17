ba31aace72cafb433d8a52374230fb2e10696d9d
whsun <449832408@qq.com>
2016-04-17 23:20:30 +0800

    add static method

--- Hello.java
+++ Hello.java
@@ -2,5 +2,13 @@ public Class Hello{
 	public static void main(String[] args){
 		System.out.println("Happy New Year sun");
 		System.out.println("Happy MySelf sun");
+		print();
+	}
+
+	public static void print()
+	{
+		System.out.println("**********************");
+		System.out.println("*	Hello World!	*");
+		System.out.println("**********************");
 	}
 }
